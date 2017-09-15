7d9bc05356ee3d08197ff78fcecc4e589a59e892
dennis williams <dennis.williams@estafet.com>
2017-09-15 14:46:08 +0100

    Initial Commit.

---
+++ src/main/java/com/estafet/microservices/api/sprint/burndown/SprintBurndownController.java
@@ -0,0 +1,8 @@
+package com.estafet.microservices.api.sprint.burndown;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SprintBurndownController {
+
+}
