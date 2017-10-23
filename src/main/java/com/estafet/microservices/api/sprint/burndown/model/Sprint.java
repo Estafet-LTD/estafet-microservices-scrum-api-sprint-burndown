2ea44fa78e669d87b1ddebd2ea391587b38b4935
dennis williams <dennis.williams@estafet.com>
2017-10-23 23:51:46 +0100

    fixed parenthethsis

--- src/main/java/com/estafet/microservices/api/sprint/burndown/model/Sprint.java
+++ src/main/java/com/estafet/microservices/api/sprint/burndown/model/Sprint.java
@@ -57,7 +57,7 @@ public class Sprint {
 		sprintDays.get(0).setIdealHours((float)initialTotalHours);
 		sprintDays.get(sprintDays.size()-1).setIdealHours(0f);
 		for (int i=1; i < sprintDays.size() - 1; i++) {
-			float ideal = (float)initialTotalHours - ((float)initialTotalHours / i+1f);
+			float ideal = (float)initialTotalHours - ((float)initialTotalHours / (i+1f));
 			sprintDays.get(i).setIdealHours(ideal);
 		}
 		return this;
