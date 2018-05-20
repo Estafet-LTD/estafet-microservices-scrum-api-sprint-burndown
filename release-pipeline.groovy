0c8d1355526de37c45eb052cc2ebdf367f2c69a1
Dennis Williams <dennis.williams@estafet.com>
2018-05-20 11:49:44 +0100

    implementation of release pipeline

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -1,8 +1,85 @@
-node() {
+@NonCPS
+def getImage(json, microservice) {
+	def item = new groovy.json.JsonSlurper().parseText(json).items.find{it.metadata.name == microservice}
+	return item.status.dockerImageRepository
+}
+
+@NonCPS
+boolean deploymentConfigurationExists(json, microservice) {
+	return new groovy.json.JsonSlurper().parseText(json).items.find{it.metadata.name == microservice} != null
+}
+
+def username() {
+    withCredentials([usernamePassword(credentialsId: 'microservices-scrum', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        return USERNAME
+    }
+}
+
+def password() {
+    withCredentials([usernamePassword(credentialsId: 'microservices-scrum', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        return PASSWORD
+    }
+}
+
+def project = "test"
+def microservice = "sprint-burndown"
+
+def developmentVersion
+def releaseVersion
+
+node('maven') {
 
 	stage("checkout") {
-		
+		git branch: "master", url: "https://${username()}:${password()}@github.com/Estafet-LTD/estafet-microservices-scrum-api-sprint-burndown"
+	}
+	
+	stage("increment version") {
+		def pom = readFile('pom.xml');
+		def matcher = new XmlSlurper().parseText(pom).version =~ /(\d+\.\d+\.)(\d+)(\-SNAPSHOT)/
+		developmentVersion = "${matcher[0][1]}${matcher[0][2].toInteger()+1}-SNAPSHOT"
+		releaseVersion = "${matcher[0][1]}${matcher[0][2]}"
+	}
+	
+	stage("perform release") {
+        sh "git config --global user.email \"jenkins@estafet.com\""
+        sh "git config --global user.name \"jenkins\""
+        withMaven(mavenSettingsConfig: 'microservices-scrum') {
+			sh "mvn release:clean release:prepare release:perform -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize -B"
+			sh "git push origin master"
+		} 
+	}	
+
+	stage("tag container with release version") {
+		openshiftTag namespace: project, srcStream: microservice, srcTag: 'PrepareForTesting', destinationNamespace: 'prod', destinationStream: microservice, destinationTag: releaseVersion
 	}
 
+	stage("update database") {
+		sh "oc get pods --selector app=postgresql -o json -n ${project} > pods.json"
+		def json = readFile('pods.json')
+		def pod = new groovy.json.JsonSlurper().parseText(json).items[0].metadata.name
+		sh "oc rsync --no-perms=true --include=\"*.ddl\" --exclude=\"*\" ./ ${pod}:/tmp -n ${project}"	
+		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"psql -d ${microservice} -U postgres -f /tmp/drop-${microservice}-db.ddl\""
+		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"psql -d ${microservice} -U postgres -f /tmp/create-${microservice}-db.ddl\""
+	}	
+	
+	stage("deploy container") {
+		sh "oc get is -o json -n ${project} > is.json"
+		def is = readFile('is.json')
+		def image = getImage (is, microservice)
+		def template = readFile ('test-deployment-config.json').replaceAll(/\$\{image\}/, image).replaceAll(/\$\{microservice\}/, microservice)
+		sh "oc get dc -o json -n test > dc.json"
+		def dc = readFile ('dc.json')
+		if (deploymentConfigurationExists (dc, microservice)) {
+			openshiftDeploy namespace: project, depCfg: microservice
+		} else {
+			openshiftCreateResource namespace:project, jsonyaml:template
+		}
+		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"
+	}	
+	
+	stage("execute acceptance tests") {
+		sh "oc start-build qa-pipeline -n cicd"	
+	}
+	
 }
 
