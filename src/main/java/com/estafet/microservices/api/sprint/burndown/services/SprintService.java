d6f3e649271b3bf1295be6ae21c0a4a9d071f242
Dennis Williams <dennis.williams@estafet.com>
2018-05-24 23:53:12 +0100

    removed sprint-api rest dependency and now uses java class (which will eventually be moved into a shared libary)

--- src/main/java/com/estafet/microservices/api/sprint/burndown/services/SprintService.java
+++ src/main/java/com/estafet/microservices/api/sprint/burndown/services/SprintService.java
@@ -1,15 +1,13 @@
 package com.estafet.microservices.api.sprint.burndown.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 import com.estafet.microservices.api.sprint.burndown.dao.SprintBurndownDAO;
 import com.estafet.microservices.api.sprint.burndown.dao.StoryDAO;
 import com.estafet.microservices.api.sprint.burndown.dao.TaskDAO;
+import com.estafet.microservices.api.sprint.burndown.date.DateHelper;
 import com.estafet.microservices.api.sprint.burndown.model.Sprint;
 import com.estafet.microservices.api.sprint.burndown.model.Story;
 import com.estafet.microservices.api.sprint.burndown.model.Task;
@@ -19,32 +17,24 @@ public class SprintService {
 
 	@Autowired
 	private SprintBurndownDAO sprintBurndownDAO;
-	
+
 	@Autowired
 	private TaskDAO taskDAO;
-	
+
 	@Autowired
 	private StoryDAO storyDAO;
-	
-	@Autowired
-	private RestTemplate restTemplate;
-	
-	@SuppressWarnings("unchecked")
-	public List<String> getSprintDays(int sprintId) {
-		return restTemplate.getForObject(System.getenv("SPRINT_API_SERVICE_URI") + "/sprint/{id}/days",
-				List.class, sprintId);
-	}
-		
+
 	@Transactional(readOnly = true)
 	public Sprint getSprintBurndown(int sprintId) {
 		Sprint sprintBurndown = sprintBurndownDAO.getSprintBurndown(sprintId);
 		return sprintBurndown.init();
 	}
-	
+
 	@Transactional
 	public void newSprint(Sprint sprint) {
 		if (sprintBurndownDAO.getSprintBurndown(sprint.getId()) == null) {
-			sprintBurndownDAO.create(sprint.addDays(getSprintDays(sprint.getId())));
+			sprintBurndownDAO
+					.create(sprint.addDays(DateHelper.getSprintDays(sprint.getStartDate(), sprint.getNoDays())));
 		}
 	}
 
@@ -78,5 +68,5 @@ public class SprintService {
 	public void newStory(Story story) {
 		storyDAO.create(story);
 	}
-	
+
 }
